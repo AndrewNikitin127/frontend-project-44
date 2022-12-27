fc104472b7204cc72763bf29b1a8b1c997cf3b95
android127 <donandrain153@gmail.com>
2022-12-27 12:42:42 +0300

    litle fix

--- src/even.js
+++ src/even.js
@@ -1,21 +1,18 @@
+/* eslint-disable no-unused-expressions */
 import readlineSync from 'readline-sync';
 
 export default function evenGame() {
-  const randomInteger = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
+  const randomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
   const maxCountOfTries = 3;
 
   const name = readlineSync.question(
-    'Welcome to the Brain Games!\nMay I have your name?: '
+    'Welcome to the Brain Games!\nMay I have your name?: ',
   );
 
-  console.log(
-    `Hello, ${name}!\nAnswer "yes" if the number is even, otherwise answer "no".`
-  );
+  console.log(`Hello, ${name}!\nAnswer "yes" if the number is even, otherwise answer "no".`);
 
-  for (let count = 1; count <= maxCountOfTries; count++) {
+  for (let count = 1; count <= maxCountOfTries; count += 1) {
     const randomInt = randomInteger(0, 100);
     console.log(`Question: ${randomInt}`);
 
@@ -26,9 +23,7 @@ export default function evenGame() {
       console.log('Correct!');
       count === maxCountOfTries ? console.log(`Congratulations, ${name}!`) : '';
     } else {
-      console.log(
-        `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`
-      );
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
       break;
     }
   }
