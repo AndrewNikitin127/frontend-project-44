609c68f96221910ae386354a97ba68080fd7d7f2
android127 <donandrain153@gmail.com>
2022-12-27 11:48:57 +0300

    add brain-even game

---
+++ src/even.js
@@ -0,0 +1,35 @@
+import readlineSync from 'readline-sync';
+
+export default function evenGame() {
+  const randomInteger = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const maxCountOfTries = 3;
+
+  const name = readlineSync.question(
+    'Welcome to the Brain Games!\nMay I have your name?: '
+  );
+
+  console.log(
+    `Hello, ${name}!\nAnswer "yes" if the number is even, otherwise answer "no".`
+  );
+
+  for (let count = 1; count <= maxCountOfTries; count++) {
+    const randomInt = randomInteger(0, 100);
+    console.log(`Question: ${randomInt}`);
+
+    const answer = readlineSync.question('Your answer: ');
+    const correctAnswer = randomInt % 2 !== 0 ? 'yes' : 'no';
+
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+      count === maxCountOfTries ? console.log(`Congratulations, ${name}!`) : '';
+    } else {
+      console.log(
+        `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`
+      );
+      break;
+    }
+  }
+}
